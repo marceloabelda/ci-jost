fd824d2802556b8ba5cb76819aac0e06650c39c3
marceloabelda <marceloabelda@gmail.com>
2011-07-15 19:45:41 -0300

    auth

---
+++ application/modules/admin/models/admin_model.php
@@ -0,0 +1,26 @@
+<?php
+
+class Admin_model extends CI_Model {
+	public $user;
+	public $dominio;
+	function __construct()
+	{
+
+	}
+
+	public function verify_user($email, $password)
+	{
+		//return true;
+		if (pam_auth($email, $password)) {
+			 
+			 $mail = explode("@",$email);
+			 $this->user = $mail[0];
+			 $this->dominio = $mail[1];
+			 return true;
+		}
+	return false;
+
+	}
+
+}
+
