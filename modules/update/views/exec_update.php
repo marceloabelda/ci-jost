3ca5c54ca283f83d65491b4690516eac8c2efb1b
marceloabelda <marceloabelda@gmail.com>
2011-09-13 18:24:10 -0300

    probando update 6

--- modules/update/views/exec_update.php
+++ modules/update/views/exec_update.php
@@ -1,11 +1,14 @@
 <?php
 	$path= BASEPATH.'/../';
 	exec('cd $path');
+	/*
 	if (ENVIRONMENT=='development') {
 		$output='git pull origin master';
 	} else {
 		exec('git status', $output);
 	}
+	*/
+	exec('git pull origin master', $output);
 	echo "<pre>";
 	print_r( $output);
 	echo "</pre>";
