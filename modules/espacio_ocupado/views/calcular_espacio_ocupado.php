1d95dccce724c0278cc4f44fe0d261187e0210b8
marceloabelda <marceloabelda@gmail.com>
2011-09-13 18:28:41 -0300

    probando update 6

--- modules/espacio_ocupado/views/calcular_espacio_ocupado.php
+++ modules/espacio_ocupado/views/calcular_espacio_ocupado.php
@@ -1,10 +1,11 @@
 <?php
-if (ENVIRONMENT=='development') {
-	$path= '/home/marcelo/Escritorio/*';
-} else {
-	$path=  '/home/*';
-}
-exec('du -csh '.$path, $output);
-echo "<pre>";
-print_r( $output);
-echo "</pre>";
+
+	if (ENVIRONMENT=='development') {
+		$path= '/home/marcelo/Escritorio/*';
+	} else {
+		$path=  '/home/*';
+	}
+	exec('du -csh '.$path, $output);
+	echo "<pre>";
+	print_r( $output);
+	echo "</pre>";
